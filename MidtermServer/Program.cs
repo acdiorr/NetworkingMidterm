1064fb501f9617a4a11c537b263ac1bd1d7d5618
Ricardo <ricardojp2511@gmail.com>
2023-03-18 19:48:35 -0400

    Added Project for Server

---
+++ MidtermServer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// Midterm For Networking
+/// Students: Ricardo Prato 100787893, Ryan Dinh _____
+/// This is the server that relays infomration to a unity project  
+
+namespace MidtermServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
